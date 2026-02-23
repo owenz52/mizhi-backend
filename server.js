5c575c063b9d42a98c2a63835a5dc6da618b02cf
靓仔 <owenz@liangzaideMacBook-Pro.local>
2026-02-23 20:43:20 +1100

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/order", (req, res) => {
+    console.log("收到订单：");
+    console.log(req.body);
+
+    res.json({ message: "订单收到！" });
+});
+
+app.listen(3000, () => {
+    console.log("服务器运行在 http://localhost:3000");
+});
